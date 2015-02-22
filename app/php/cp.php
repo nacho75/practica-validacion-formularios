1031d41eb58e85e7398655315ee281168e105c52
nacho75 <imartinezdezabarte@gmail.com>
2015-02-22 20:12:23 +0100

    Commit4 validando validando

--- app/php/cp.php
+++ app/php/cp.php
@@ -1,10 +1,12 @@
 <?php
 /* Descomentaríamos la siguiente línea para mostrar errores de php en el fichero: */
-//ini_set('display_errors', '1');
+// ini_set('display_errors', '1');
 /* Definimos los parámetros de conexión con la bbdd: */
-$dbinfo = "mysql:dbname=validacion;host=localhost";
+
+$dbinfo = "mysql:dbname=provincias;host=localhost";
 $user = "root";
 $pass = "root";
+
 //Nos intentamos conectar:
 try {
     /* conectamos con bbdd e inicializamos conexión como UTF8 */
@@ -14,13 +16,18 @@ try {
     echo "La conexi&oacute;n ha fallado: " . $e->getMessage();
 }
 /* Para hacer debug cargaríamos a mano el parámetro, descomentaríamos la siguiente línea: */
-//$_REQUEST['nif'] = "73003600A";
-if (isset($_REQUEST['documentNumber'])) {
+//$_REQUEST['zip'] = "12";
+$prueba=$_POST['zip'];
+echo"$prueba";
+if (isset($_POST['zip'])) {
     /* La línea siguiente la podemos descomentar para ver desde firebug-xhr si se pasa bien el parámetro desde el formulario */
-    //echo $_REQUEST['nif'];
-    $nif = $_REQUEST['documentNumber'];
-    $sql = $db->prepare("SELECT * FROM usuarios WHERE nif=?");
-    $sql->bindParam(1, $nif, PDO::PARAM_STR);
+    //echo $_REQUEST['email'];
+    ?>
+
+    <?php
+   
+    $sql = $db->prepare("SELECT Municipio,CodPostal FROM t_municipios WHERE CodPostal=?");
+    $sql->bindParam(1,$prueba);
     $sql->execute();
     /* Ojo... PDOStatement::rowCount() devuelve el número de filas afectadas por la última sentencia DELETE, INSERT, o UPDATE 
      * ejecutada por el correspondiente objeto PDOStatement.Si la última sentencia SQL ejecutada por el objeto PDOStatement 
@@ -28,14 +35,33 @@ if (isset($_REQUEST['documentNumber'])) {
      * Sin embargo, este comportamiento no está garantizado para todas las bases de datos y no debería confiarse en él para 
      * aplicaciones portables.
      */
-    $valid = 'true';
+    /*
+    $valid = 'true'; 
     if ($sql->rowCount() > 0) {
         $valid= 'false';
     } else {
        $valid='true';
     }
+    
+    */
+   // $okey = $sql->fetch(); 
+       ?>
+ 
+    <?php  
+    while ($row=$sql->fetch()) {   
+           ?>
+
+    <?php
+    $opciones.= "<option ='{$row['CodPostal']}'>{$row['Municipio']}</option>";
+  
+    
+      }
+     
+    echo $opciones;
+   
+    
 }
 $sql=null;
 $db = null;
-echo $valid;
+echo $okey[0];
 ?>
